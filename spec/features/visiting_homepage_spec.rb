554ce7eb9862fb6b6315e2564c26cde87894a1da
Ryan Chang <ry4nch4ng@gmail.com>
2016-12-22 03:21:49 +0000

    add bootstrap to homepage

--- spec/features/visiting_homepage_spec.rb
+++ spec/features/visiting_homepage_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Visiting homepage" do
   scenario "user visits homepage" do
     visit "/"
     expect(page).to have_link("Home")
-    expect(page).to have_link("Athlete's Den")
+    expect(page).to have_link("Get Swole Land")
     expect(page).to have_content("Workout Lounge")
     expect(page).to have_content("Show off your workout")
   end
